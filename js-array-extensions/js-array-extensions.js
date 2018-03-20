16bae55c5fa7271a66b02a23c6f6f3bfd99f4b25
Salsabeel Jaafreh <s.jaafreh@yahoo.com>
2018-03-20 20:06:11 +0200

    Update js-array-extensions.js

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -2,15 +2,15 @@
 var Array=function(){
  this._arr1=new Array();
 this._methods={}
- ._extend(this._arr1,this._methods)
+
 };
 
-this._methods.first=function(){
+this._methods.prototype.first=function(){
 	return this._arr1[0];
 
 };
 
-this._methods.last=function(){
+this._methods.protootype.last=function(){
 	return this._arr1[this._arr1.length-1]
 
 };
